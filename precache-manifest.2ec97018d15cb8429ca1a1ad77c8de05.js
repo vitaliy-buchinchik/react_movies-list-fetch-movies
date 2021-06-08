36723e6bdc54a9db0e2006b7d887115bbb7fdc2f
Vitaliy Buchincik <v.buchinchik@gmail.com>
2021-06-08 08:42:36 +0300

    Updates

---
+++ precache-manifest.2ec97018d15cb8429ca1a1ad77c8de05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3819277e7fdaae1203272d37cfe14107",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1d051dfcc0c525bb4ec",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "c6bf41d8d8e34c1b5cfe",
+    "url": "./static/css/main.9aaf0c5f.chunk.css"
+  },
+  {
+    "revision": "d1d051dfcc0c525bb4ec",
+    "url": "./static/js/2.28065652.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.28065652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bf41d8d8e34c1b5cfe",
+    "url": "./static/js/main.9f28ec9f.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
